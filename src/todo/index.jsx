16b674209f1b40a763e3d4188a8d46ec5c317ea0
william <willshinji@gmail.com>
2019-06-30 10:21:46 -0300

    initial files

---
+++ src/todo/index.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Todo extends Component {
+    render(){
+        return (
+            <>
+                <h1>Todo</h1>
+            </>
+        )
+    }
+}
+
+export default Todo
