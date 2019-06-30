6331f2edc187b21fc38130d7c09a121c6a55aecb
william <willshinji@gmail.com>
2019-06-30 10:24:27 -0300

    create form structure

---
+++ src/todo/form.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Form = props => (
+    <div>
+        <input type='text' placeholder='todo' />
+        <button type='button'>Create</button>
+    </div>
+)
+
+export default Form
--- src/todo/index.jsx
+++ src/todo/index.jsx
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
+import Form from './form'
 
 class Todo extends Component {
     render(){
         return (
             <>
                 <h1>Todo</h1>
+                <Form />
             </>
         )
     }
