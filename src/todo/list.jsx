3dfc2b2de4fd677eee1055e9110f830b7a6c7049
william <willshinji@gmail.com>
2019-07-03 22:23:21 -0300

    minor refactor

--- src/todo/list.jsx
+++ src/todo/list.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
-const List = props => (
-    <div>{props.todo.list.map(l => l.description)}</div>
-)
+const List = props => {
+    const { list } = props.todo
+    return (
+        <div>
+            <ol>
+                {list.map((l, index) => <li key={index}>{l.description}</li>)}
+            </ol>
+        </div>
+    )
+}
 
-const mapStateToProps = state => ({todo: state.todo})
+const mapStateToProps = state => ({ todo: state.todo })
 export default connect(mapStateToProps)(List)
