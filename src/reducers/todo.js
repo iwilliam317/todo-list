22761e9f4690095f31155a1c5fc54fea82ab280d
william <willshinji@gmail.com>
2019-07-05 22:26:25 -0300

    refactor reducers

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,23 +1,24 @@
 const INITIAL_STATE = {list: []}
 
 const TodoReducer = (state = INITIAL_STATE, action) => {
+   let list = [] 
     switch(action.type){
         case 'HANDLE_CHANGE':
             return {...state, description: action.payload}
         case 'SAVE_TODO':
             const id = Math.floor(Math.random() * 100);  
             const todo = {id, description: action.payload, done: false}
-            const aux = state.list
-            aux.push(todo)
-            return {...state, list: aux}
+            list = state.list
+            list.push(todo)
+            return {...state}
         case 'REMOVE_TODO':
             const todoId = action.payload.id || 0
             const index = state.list.find((t, index) => {
                 if(t === todoId) return index
             })
-            const aux2 = state.list
-            aux2.pop(index)
-            return {...state, list: aux2}
+            list = state.list
+            list.pop(index)
+            return {...state, list}
         default:
             return state
     }
