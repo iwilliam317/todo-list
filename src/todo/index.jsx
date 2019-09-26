c3e469de46ad1dd3f6f69d70ef6289fac475ee2a
william <willshinji@gmail.com>
2019-09-26 10:54:04 -0300

    add author info

--- src/todo/index.jsx
+++ src/todo/index.jsx
@@ -11,6 +11,9 @@ class Todo extends Component {
                 </div>
                 <Form />
                 <List />
+                <div className='rights'>
+                    <p><small>Created by <a href="https://github.com/iwilliam317">William Shinji</a></small></p>
+                </div>
             </>
         )
     }
