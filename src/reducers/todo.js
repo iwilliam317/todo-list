b5abd4eaa0daca6c3b5351b75238242ec58f66ce
william <willshinji@gmail.com>
2019-10-02 11:49:24 -0300

    add localstorage

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,17 +1,21 @@
-const INITIAL_STATE = {list: []}
+const INITIAL_STATE = {list: JSON.parse(localStorage.getItem('list')) || []}
 
 const TodoReducer = (state = INITIAL_STATE, action) => {
    let list = [] 
    let todoId
     switch(action.type){
+
         case 'HANDLE_CHANGE':
             return {...state, description: action.payload}
+
         case 'SAVE_TODO':
             const id = Math.floor(Math.random() * 100);  
             const todo = {id, description: action.payload, done: false}
             list = state.list
             list.push(todo)
+            localStorage.setItem('list', JSON.stringify(list))
             return {...state, description: ''}
+
         case 'REMOVE_TODO':
             todoId = action.payload.id || 0
             const index = state.list.find((t, index) => {
@@ -20,6 +24,7 @@ const TodoReducer = (state = INITIAL_STATE, action) => {
             list = state.list
             list.pop(index)
             return {...state, list}
+
         case 'TODO_DONE':
             todoId = action.payload.id || 0
             list = state.list
@@ -29,6 +34,7 @@ const TodoReducer = (state = INITIAL_STATE, action) => {
                 } 
             })
             return {...state, list}
+
         default:
             return state
     }
